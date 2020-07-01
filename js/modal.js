d5b9fd06f54c0cfbbcefd7c4ac16392136b4abff
Oleksandra Mashchenko <dremka@Oleksandras-MacBook-Pro.local>
2020-07-01 19:50:36 +0300

    init

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+};
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+refs.modal.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return;
+  e.currentTarget.classList.add('is-hidden');
+});
+
+function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+}
